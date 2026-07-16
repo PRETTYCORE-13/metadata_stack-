8a6f30487a714822d557676be521154a3ac72f10
PRETTYCORE <ennovacore.com.mx.goo@gmail.com>
2026-07-16 14:06:56 -0600

    Agrega catálogo pty_motocicletas (API + autómata + reglas)

---
+++ lib/metadata_app/meta_business_process/reglas/pty_motocicletas/alta_pre.ex
@@ -0,0 +1,9 @@
+defmodule MetadataApp.MetaBusinessProcess.Reglas.PtyMotocicletas.AltaPre do
+  @behaviour MetadataApp.MetaStateEngine.ReglaPre
+
+  @impl true
+  def evaluar(_registro, _contexto, _params) do
+    # ESCRIBA SUS REGLAS AQUI
+    :ok
+  end
+end
---
+++ lib/metadata_app/meta_business_process/reglas/pty_motocicletas/dar_de_baja_pre.ex
@@ -0,0 +1,9 @@
+defmodule MetadataApp.MetaBusinessProcess.Reglas.PtyMotocicletas.DarDeBajaPre do
+  @behaviour MetadataApp.MetaStateEngine.ReglaPre
+
+  @impl true
+  def evaluar(_registro, _contexto, _params) do
+    # ESCRIBA SUS REGLAS AQUI
+    :ok
+  end
+end
