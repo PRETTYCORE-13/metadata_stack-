d79bdc97a83adb03c16828443b65fb8380d6c7ab
Jesus Sanchez <jesusaguillaryt@gmail.com>
2026-08-03 12:49:03 -0600

    Saca del repo los catalogos demo100 generados (deben ser locales, no compartidos)
    Estos catalogos/migraciones son BCs armados por el BPB, igual que los
    pty_* que ya estaban excluidos por el mismo motivo (ver .gitignore):
    cada desarrollador genera su propio set local con "mix gen.catalogos" a
    partir de meta_schema_*, asi que subirlos al repo compartido genera
    conflictos de version entre quienes no tienen el mismo set armado.
    Sigue en pie el selector de campos con drag-and-drop (catalogo_live.ex,
    app.js) del commit anterior, eso no se toca.

--- lib/metadata_app/meta_business_process/catalogos/demo100_inventario_productos_materia_prima_reg.ex
+++
@@ -1,3 +0,0 @@
-defmodule MetadataApp.MetaBusinessProcess.Catalogos.Demo100InventarioProductosMateriaPrimaReg do
-  use MetadataApp.BusinessProcessBuilder.MetaCatalogoGenerico, tabla: "demo100_inventario_productos_materia_prima_reg", campos: [{:nombre, :string, %{formato: nil, longitud: 255, opcional: false, valor_default: nil}}, {:descripcion, :string, %{formato: nil, longitud: 255, opcional: false, valor_default: nil}}, {:activo, :boolean, %{opcional: false, valor_default: nil}}, {:cantidad, :integer, %{maximo: nil, minimo: nil, opcional: false, valor_default: nil}}]
-end
--- lib/metadata_app/meta_business_process/catalogos/demo100_rrhh_capacitacion_internas_reg.ex
+++
@@ -1,3 +0,0 @@
-defmodule MetadataApp.MetaBusinessProcess.Catalogos.Demo100RrhhCapacitacionInternasReg do
-  use MetadataApp.BusinessProcessBuilder.MetaCatalogoGenerico, tabla: "demo100_rrhh_capacitacion_internas_reg", campos: [{:nombre, :string, %{formato: nil, longitud: 255, opcional: false, valor_default: nil}}, {:descripcion, :string, %{formato: nil, longitud: 255, opcional: false, valor_default: nil}}, {:activo, :boolean, %{opcional: false, valor_default: nil}}, {:cantidad, :integer, %{maximo: nil, minimo: nil, opcional: false, valor_default: nil}}]
-end
